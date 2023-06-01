06560adc2602fc34c7b8f3fc4a6f282246a92fb9
Amir Kooshky <kooshkya@gmail.com>
2023-06-01 10:10:31 +0330

    first try

--- predictor.v
+++ predictor.v
@@ -1,5 +1,21 @@
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-// Your code
+reg [1:0] memory;
+
+initial begin
+    memory = {1, 1};
+end
+
+always @ (posedge clk) begin
+    if (request) begin
+        prediction = memory[1];
+    end else if (result) begin
+        if (~taken && |memory) begin
+            memory -= 1;
+        end else if (taken && |(~memory)) begin
+            memory += 1;
+        end
+    end
+end
 
 endmodule
